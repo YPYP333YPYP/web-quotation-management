db1631834ac81ce11f75e0251d03d82d5d6caf02
YPYP333YPYP <ypyp333ypyp@gmail.com>
2024-07-10 05:09:52 +0900

    refactor: 로그 관련 코드 수정

--- core/logging/config.py
+++ core/logging/config.py
@@ -1,40 +1,44 @@
 import logging
 import os
+import queue
 from datetime import datetime
-from logging.handlers import TimedRotatingFileHandler
-
-logging.basicConfig(
-    filename='error.log',
-    level=logging.ERROR,
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-)
-
-log_dir = 'error'
-if not os.path.exists(log_dir):
-    os.makedirs(log_dir)
+from logging.handlers import TimedRotatingFileHandler, QueueHandler, QueueListener
 
 
+log_dir = os.path.join(os.getcwd(), 'error')
+os.makedirs(log_dir, exist_ok=True)
 log_file = os.path.join(log_dir, 'error.log')
 
-handler = TimedRotatingFileHandler(
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+
+def namer(name):
+    base_filename = os.path.basename(name)
+    dir_name = os.path.dirname(name)
+    date_str = datetime.now().strftime("%Y-%m-%d")
+    return os.path.join(dir_name, f"{os.path.splitext(base_filename)[0]}-{date_str}.log")
+
+
+file_handler = TimedRotatingFileHandler(
     log_file,
     when="midnight",
     interval=1,
     backupCount=30,
     encoding='utf-8'
 )
-
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-
-# 로거 설정
-logger = logging.getLogger()
-logger.setLevel(logging.ERROR)
-logger.addHandler(handler)
+file_handler.setFormatter(formatter)
+file_handler.namer = namer
 
 
-def namer(name):
-    return name.replace(".log", "") + "-" + datetime.now().strftime("%Y-%m-%d") + ".log"
+log_queue = queue.Queue(-1)
+queue_handler = QueueHandler(log_queue)
+
+root_logger = logging.getLogger()
+root_logger.setLevel(logging.ERROR)  # 필요에 따라 DEBUG로 변경
+root_logger.addHandler(queue_handler)
 
 
-handler.namer = namer
+listener = QueueListener(log_queue, file_handler, respect_handler_level=True)
+listener.start()
+
+
--- main.py
+++ main.py
@@ -1,15 +1,36 @@
+import logging
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 from fastapi.exceptions import ResponseValidationError
 from starlette.middleware.cors import CORSMiddleware
 import uvicorn
 
 from api import router
+from core.logging.config import listener
 from core.response.handler.exception_handler import GeneralException, general_exception_handler, \
     validation_exception_handler
 from core.middleware import RequestMiddleware
 
+logger = logging.getLogger()
+
+
+# 비동기 컨텍스트 관리자를 생성하는 데코레이터
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # 시작 시 실행될 코드
+    try:
+        raise Exception("Test Error")
+    except Exception as e:
+        logging.error(f"Test error occurred: {str(e)}")
+        print(f"Test error logged: {str(e)}")
+
+    yield
+    listener.stop()
+
+
 def get_application() -> FastAPI:
-    application = FastAPI()
+    application = FastAPI(lifespan=lifespan)
     application.include_router(router)
 
     origins = ["*"]
@@ -29,6 +50,8 @@ def get_application() -> FastAPI:
 
 
 app = get_application()
+
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000, log_level="debug")
