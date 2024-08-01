7bb7eca78840d466648367c259b0fdc5c264cefd
YPYP333YPYP <ypyp333ypyp@gmail.com>
2024-08-02 07:06:52 +0900

    fix: jwt 토큰 발행 시간 추가

--- core/settings.py
+++ core/settings.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 class JwtTokenSettings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 3000
 
     class Config:
         env_file = ".env"
