35429f549be1383174ce76c5fc1a98ca25fae235
YPYP333YPYP <ypyp333ypyp@gmail.com>
2024-08-07 11:37:10 +0900

    fix: jwt 토큰 시간 추가

--- core/config.py
+++ core/config.py
@@ -13,7 +13,7 @@ mysql_settings = MySQLSettings(
 jwt_settings = JwtTokenSettings(
     SECRET_KEY=os.getenv('SECRET_KEY'),
     ALGORITHM=os.getenv('ALGORITHM'),
-    ACCESS_TOKEN_EXPIRE_MINUTES=int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+    ACCESS_TOKEN_EXPIRE_MINUTES=int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 )
 
 
